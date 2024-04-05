2ef45c24576c229515dac50d156f5ed1169b5f1f
Michael <michaelcloran2010@gmail.com>
2024-04-05 07:27:00 +0100

    feat: readied code for deployment to heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u#wqml4hqk*f)@*$(gmyc_zqg=*cayo(!xpa7#j51@8a(tp#t9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
